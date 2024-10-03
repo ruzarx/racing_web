8c2fab1e91d1d0967b7f280a848b214a3720bb19
ruzarx <ruzarx@gmail.com>
2024-10-03 17:29:05 +0400

    Chromium options

--- scrapper/db_scrapper.py
+++ scrapper/db_scrapper.py
@@ -15,7 +15,12 @@ def scrap_race(season: int, race_number: int) -> bool:
     url_race_number = str(race_number) if len(str(race_number)) == 2 else f"0{race_number}"
     url = f'https://www.racing-reference.info/race/{season}-{url_race_number}/W'
     options = Options()
-    options.headless = True
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--remote-debugging-port=9222")
     driver = webdriver.Chrome(options=options)
 
     try:
