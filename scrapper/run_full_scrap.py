795226686c73c494cf9bbf54571ff1df61f886e1
ruzarx <ruzarx@gmail.com>
2024-10-04 15:05:57 +0400

    Cron for scrapping

--- scrapper/run_full_scrap.py
+++ scrapper/run_full_scrap.py
@@ -6,24 +6,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# writer = DBWriter(app, 'postgres:postgres@localhost:5432/racing_db')
+writer = DBWriter(app, 'postgres:postgres@localhost:5432/racing_db')
 
-# writer.fill_tracks_info()
+writer.fill_tracks_info()
 
-# available_races = writer.get_available_races()
+available_races = writer.get_available_races()
 
-# writer.fill_calendar_info()
+writer.fill_calendar_info()
 
 season = 2024
-for race_number in range(1, 40):
-    # if (season, race_number) not in available_races:
-    print(season, race_number)
-    is_success = scrap_race(season, race_number)
-        # if not is_success:
-        #     break
-    race_data = NascarRaceDataParser(season, race_number).fill_race_data()
-    print(race_data)
-        # writer.fill_race_data(race_data)
-        # race_results, standings = NascarResultsParser(season, race_number).fill_results_data()
-        # writer.fill_race_results(race_results)
-        # writer.fill_standings(standings)
+for season in (2023, 2024):
+    for race_number in range(40):
+        if (season, race_number) not in available_races:
+            print(season, race_number)
+            is_success = scrap_race(season, race_number)
+            if not is_success:
+                break
+            race_data = NascarRaceDataParser(season, race_number).fill_race_data()
+            writer.fill_race_data(race_data)
+            race_results, standings = NascarResultsParser(season, race_number).fill_results_data()
+            writer.fill_race_results(race_results)
+            writer.fill_standings(standings)
